e074228458c12d5038b02d5f6e029a87d267a583
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-09-24 22:53:29 -0500

    comment posting working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require('./api/server.js');
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 6000;
 server.listen(PORT, () => {
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -74,7 +74,32 @@ router.get('/:user_id/posts/:post_id/instructions', (req, res) => {
               if (instructions) {
                 res.json(instructions);
               } else {
-                res.status(404).json({ message: 'Could not find posts for given user' })
+                res.status(404).json({ message: 'Could not find posts for given post' })
+              }
+            })
+      })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get posts' });
+  });
+});
+
+router.get('/:user_id/posts/:post_id/comments', (req, res) => {
+  const { user_id } = req.params;
+  const { post_id } = req.params;
+
+  Users.findPosts(user_id)
+  .then(posts => {
+    if (posts) {
+      Users.findPostById(post_id)
+      .then(post => {
+          Users.findComments(post_id)
+            .then(comments => {
+              if (comments) {
+                res.json(comments);
+              } else {
+                res.status(404).json({ message: 'Could not find comments for given post' })
               }
             })
       })
@@ -127,6 +152,53 @@ router.post('/:id/posts', (req, res) => {
   });
 });
 
+router.post('/:user_id/posts/:post_id/comments', (req, res) => {
+  const  commentData  = req.body;
+  const { user_id } = req.params;
+  const { post_id } = req.params;
+
+  Users.findPosts(user_id)
+  .then(posts => {
+    if (posts) {
+      Users.findPostById(post_id)
+      .then(post => {
+          Users.addComment(commentData,post_id)
+            .then(comment => {
+              res.status(201).json(post);
+            })  
+      })
+    } else {
+      res.status(404).json({ message: 'Could not find post with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to post comment' });
+  });
+});
+
+router.post('/:user_id/posts/:post_id/instructions', (req, res) => {
+  const  instructionData = req.body;
+  const { user_post_comment_id } = req.params;
+  const { user_post_id } = req.params;
+
+  Users.findPosts(user_post_comment_id)
+  .then(posts => {
+    if (posts) {
+      Users.findPostById(user_post_id)
+      .then(post => {
+          Users.addInstruction(instructionData, user_post_id)
+            .then(instruction => {
+              res.status(201).json(post);
+            })  
+      })
+    } else {
+      res.status(404).json({ message: 'Could not find post with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to post instruction' });
+  });
+});
 
 
 module.exports = router;
